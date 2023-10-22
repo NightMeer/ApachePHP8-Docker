0b1431cee0217577d60c356d5f46ccc53b327616
NightMeer <licht.marcel.ml@gmail.com>
2023-10-22 16:51:21 +0200

    Delete unused Code Add second Volume

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM php:8-apache
-RUN apt-get update && apt-get upgrade -y && apt-get install -y wget && apt-get install -y rsync && apt-get install -y libicu-dev && docker-php-ext-configure intl && docker-php-ext-install intl && docker-php-ext-configure mysqli && docker-php-ext-install mysqli && a2enmod rewrite
+RUN apt-get update && apt-get upgrade -y  && apt-get install -y rsync && apt-get install -y libicu-dev && docker-php-ext-configure intl && docker-php-ext-install intl && docker-php-ext-configure mysqli && docker-php-ext-install mysqli && a2enmod rewrite
 EXPOSE 80
 
 WORKDIR /var/www/html
 
 VOLUME ["/config"]
+VOLUME ["/var/www/html"]
 
 COPY ./startup.sh /startup.sh
 CMD chmod 777 /startup.sh
--- startup.sh
+++ startup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ ! -d /config/html ]; then
-    echo "HTML NOT Found"
-    cp -R /var/www/html/ /config
-else
-    echo "HTML Found"
-    cp -R /config/html/ /var/www/
-fi
+#if [ ! -d /config/html ]; then
+#    echo "HTML NOT Found"
+#    cp -R /var/www/html/ /config
+#else
+#    echo "HTML Found"
+#    cp -R /config/html/ /var/www/
+#fi
 
 if [ ! -d /config/apache2 ]; 
 then
@@ -17,7 +17,9 @@ else
     cp -R /config/apache2/ /etc/
 fi
 
-chmod -R 777 /config/html/
+#chmod -R 777 /config/html/
 chmod -R 777 /config/apache2/
+chmod -R 777 /var/www/html/
 
-while :; do rsync -au --delete /config/html /var/www; rsync -au --delete /var/www/html /config/; sleep 1; done & apache2ctl -D FOREGROUND
+#while :; do rsync -au /config/html /var/www; rsync -au /var/www/html /config/; sleep 1; done & apache2ctl -D FOREGROUND
+apache2ctl -D FOREGROUND
