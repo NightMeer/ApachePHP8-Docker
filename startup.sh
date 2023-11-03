0e26422888b0db0bca420e5b2419d5755ca6d92b
NightMeer <licht.marcel.ml@gmail.com>
2023-11-03 07:46:19 +0100

    Delete unused Code Add php.ini

--- startup.sh
+++ startup.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-#if [ ! -d /config/html ]; then
-#    echo "HTML NOT Found"
-#    cp -R /var/www/html/ /config
-#else
-#    echo "HTML Found"
-#    cp -R /config/html/ /var/www/
-#fi
-
-if [ ! -d /config/apache2 ]; 
+if [ ! -d /config/apache2 ];
 then
     echo "CONFIG NOT Found!"
     cp -R /etc/apache2/ /config
@@ -17,9 +9,18 @@ else
     cp -R /config/apache2/ /etc/
 fi
 
-#chmod -R 777 /config/html/
+if [ ! -d /config/php ];
+then
+    echo "CONFIG NOT Found!"
+    touch /usr/local/etc/php/php.ini
+    cp -R /usr/local/etc/php/ /config
+else
+    echo "CONFIG Found!"
+    cp -R /config/php/ /usr/local/etc/
+fi
+
+chmod -R 777 /config/php/
 chmod -R 777 /config/apache2/
 chmod -R 777 /var/www/html/
 
-#while :; do rsync -au /config/html /var/www; rsync -au /var/www/html /config/; sleep 1; done & apache2ctl -D FOREGROUND
 apache2ctl -D FOREGROUND
